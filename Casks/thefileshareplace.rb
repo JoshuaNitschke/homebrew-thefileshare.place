1d131bad1dd53f1e3a8296a8907c5b5aae64c63b
Joshua Nitschke <>
2026-04-03 23:24:11 -0700

    Initial cask

---
+++ Casks/thefileshareplace.rb
@@ -0,0 +1,11 @@
+cask "thefileshareplace" do
+  version "0.0.0"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+
+  url "https://downloads.thefileshare.place/v0.0.0/TheFileSharePlace-0.0.0-osx-arm64.dmg"
+  name "theFILESHARE.place"
+  desc "Share files. No accounts, no nonsense."
+  homepage "https://thefileshare.place"
+
+  app "TheFileSharePlace.app"
+end
